cfaaec6445cc78abf9bdc3fc837ff19e7ed175fc
Mason Rahmani (Pahlawan) <54111537+masonrahmani@users.noreply.github.com>
2022-02-08 11:34:28 +0430

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Mobile App Developer","Machine Learning Engineer","Entrepreneur","Freelance"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
